a6ee55e7b15231efe0b04512198367c0b277da29
tamilsabi <tamilsabi78@gmail.com>
2026-02-03 11:56:10 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,63 @@
+function o(){
+    a=document.getElementById("oo")
+    a.value+="1"
+}
+function tw(){
+    a=document.getElementById("oo")
+    a.value+="2"
+}
+function th(){
+    a=document.getElementById("oo")
+    a.value+="3"
+}
+function sla(){
+    a=document.getElementById("oo")
+    a.value+="/"
+}
+function fo(){
+    a=document.getElementById("oo")
+    a.value+="4"
+}
+function fi(){
+    a=document.getElementById("oo")
+    a.value+="5"
+}
+function si(){
+    a=document.getElementById("oo")
+    a.value+="6"
+}
+function mul(){
+    a=document.getElementById("oo")
+    a.value+="*"
+}
+function se(){
+    a=document.getElementById("oo")
+    a.value+="7"
+}
+function ei(){
+    a=document.getElementById("oo")
+    a.value+="8"
+}
+function ni(){
+    a=document.getElementById("oo")
+    a.value+="9"
+}
+function add(){
+    a=document.getElementById("oo")
+    a.value+="+"
+}
+function sub(){
+    a=document.getElementById("oo")
+    a.value+="-"
+}function zr(){
+    a=document.getElementById("oo")
+    a.value+="0"
+}function clr(){
+    a=document.getElementById("oo")
+    a.value=""
+}
+
+function calc(){
+    a=document.getElementById("oo")
+    a.value=eval(a.value)
+}
